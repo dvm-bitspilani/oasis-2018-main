ef3aef12e84c181fd5ed48d71366d5bb345156c6
kunal-mohta <kunalmohta1818@gmail.com>
2018-09-04 16:25:06 +0530

    events slider - start

---
+++ js/events.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#event-svg-container').lightSlider({
+        // gallery: true,
+        item: 1,
+        loop:true,
+        slideMargin: 0,
+        controls: false
+    });
+});
