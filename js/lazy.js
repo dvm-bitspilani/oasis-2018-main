02956f7be93fdb6bb5710bca1f9cc98078a7a85d
Laksh Singla <lakshsingla@gmail.com>
2018-09-16 01:31:25 +0530

    Change branch

---
+++ js/lazy.js
@@ -0,0 +1,48 @@
+"use strict"
+
+var lazyLoadImgFunctionGenerator = function (elems, dataProps, completeAll) {
+	return function(){
+		elems.forEach(function(elem, i) {
+			for(var dataProp in dataProps) { 
+				dataProps[dataProp](elem.dataset[dataProp], elem);
+			}
+		});
+		completeAll()
+	}
+}
+
+function lazyLoad() {
+	var lazyLoadEventImages = lazyLoadImgFunctionGenerator(Array.from(document.getElementsByClassName("events-svg")),
+		{
+			name: function(n, el) {
+				var baseURI = "/imgs/svg-anims/";
+				console.log(1);
+				console.log(baseURI + n);
+				fetch(baseURI + n, {
+					method: 'get',
+				}).then(function(res){
+					console.log(2);
+					return res.blob()
+				}).then(function(data){
+					console.log(3);
+					console.log(data);
+					var objectURL = URL.createObjectURL(data);
+					console.log(objectURL + "Hello");
+					el.setAttribute("data", objectURL);	
+					el.setAttribute("type", "image/svg+xml");
+					el.style.width = "100%";
+					el.style.height = "100%";
+				})
+			}
+		},
+		function() {
+			document.getElementById("view-events-btn").style.opacity = "1";	
+			document.getElementById("next-event").style.opacity = "1";	
+			document.getElementById("prev-event").style.opacity = "1";	
+		}
+	);
+	lazyLoadEventImages();
+}
+
+lazyLoad();
+
